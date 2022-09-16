0905f07b50091670454e1a2cf38027ffe143fec1
RussianLord <dj_volume@mail.ru>
2022-09-16 13:51:40 +0300

    доработка условий для ввода одинаковых чисел

--- First/Program.cs
+++ First/Program.cs
@@ -1,15 +1,19 @@
 ﻿Console.WriteLine("Введите ПЕРВОЕ число...");
 int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Первое число = " + A);
+Console.WriteLine("Число А = " + A);
 Console.WriteLine("Введите ВТОРОЕ число...");
 int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Второе число = " + B);
+Console.WriteLine("Число В = " + B);
 
-if(A>B)
+if(A == B)
 {
-    Console.WriteLine($"Число {A} больше числа {B}");
+Console.WriteLine($"Число A = {A} и число B = {B} равны");
+}
+else if(A>B)
+{
+    Console.WriteLine($"Число A = {A} больше числа B = {B}");
 }
 else
 {
-    Console.WriteLine($"Число {B} больше числа {A}");
+    Console.WriteLine($"Число B = {B} больше числа A = {A}");
 }
