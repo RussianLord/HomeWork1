4b7c373c9363384aae66b436989bd1aa8e08eef9
RussianLord <dj_volume@mail.ru>
2022-09-16 14:03:14 +0300

    добавил условие при введении 0 в silver

--- Silver/Program.cs
+++ Silver/Program.cs
@@ -2,7 +2,11 @@
 int A = Convert.ToInt32(Console.ReadLine());
 int B = A % 2;
 
-if (A % 2 == 0)
+if (A == 0)
+{
+    Console.WriteLine("Вы ввели 0");
+}
+else if (A % 2 == 0)
 {
     Console.WriteLine($"Число {A} является чётным.");
 }
