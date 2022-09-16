b779c0c94b864e5d997b5d9266dd3dbba6808425
RussianLord <dj_volume@mail.ru>
2022-09-16 13:35:40 +0300

    Сделал последнее задание

--- Gold/Program.cs
+++ Gold/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число...");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count <= N)
+{
+    if (count % 2 == 0)
+{
+    Console.Write(count + ",");
+}
+    count++;
+}
+Console.WriteLine("Были выведены все чётные числа");
