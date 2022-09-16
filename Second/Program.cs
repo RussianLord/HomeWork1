0892bb4d1e7ebfd52939d2ffde7d0a22b541f780
RussianLord <dj_volume@mail.ru>
2022-09-16 13:59:49 +0300

    Улучшил условие, теперь можно писать три одинаковых числа, а также два максимальных одинаковых

--- Second/Program.cs
+++ Second/Program.cs
@@ -1,18 +1,34 @@
 ﻿Console.WriteLine("Введите ПЕРВОЕ число...");
 int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Первое число = " + A);
+Console.WriteLine("Число А = " + A);
 Console.WriteLine("Введите ВТОРОЕ число...");
 int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Второе число = " + B);
+Console.WriteLine("Число В = " + B);
 Console.WriteLine("Введите ТРЕТЬЕ число...");
 int C = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Третье число = " + C);
+Console.WriteLine("Число С = " + C);
 
-if(A>B & A>C)
+if(A == B & A == C)
+{
+    Console.WriteLine("Вы ввели одинаковые числа");
+}
+else if(A == B & A > C)
+{
+    Console.WriteLine($"Число A = {A} и число B = {B} самые большые числа из списка");
+}
+else if(A == C & A > B)
+{
+    Console.WriteLine($"Число A = {A} и число C = {C} самые большые числа из списка");
+}
+else if(B == C & B > A)
+{
+    Console.WriteLine($"Число B = {B} и число C = {C} самые большые числа из списка");
+}    
+else if(A > B & A > C)
 {
     Console.WriteLine($"Число {A} самое большое число из списка");
 }
-else if(C>B & C>A)
+else if(C > B & C > A)
 {
     Console.WriteLine($"Число {C} самое большое число из списка");
 }
