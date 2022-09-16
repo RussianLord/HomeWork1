0070497fd86361ce4c8371d612e6de15c9f64609
RussianLord <dj_volume@mail.ru>
2022-09-16 13:24:52 +0300

    Сделал 3 задание в папке Silver

---
+++ Silver/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ввидете число...");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = A % 2;
+
+if (A % 2 == 0)
+{
+    Console.WriteLine($"Число {A} является чётным.");
+}
+else
+{
+    Console.WriteLine($"Число {A} не является чётным.");
+}
