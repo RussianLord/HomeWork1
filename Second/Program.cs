b4061c8d5f575dab94e83246f95c777764ebbd99
RussianLord <dj_volume@mail.ru>
2022-09-16 12:57:31 +0300

    Сделал вторую задачу

--- Second/Program.cs
+++ Second/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите ПЕРВОЕ число...");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Первое число = " + A);
+Console.WriteLine("Введите ВТОРОЕ число...");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число = " + B);
+Console.WriteLine("Введите ТРЕТЬЕ число...");
+int C = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье число = " + C);
+
+if(A>B)
+{
+    Console.WriteLine($"Число {A} самое большое число из списка");
+}
+else if(C>B)
+{
+    Console.WriteLine($"Число {C} самое большое число из списка");
+}
+else
+{
+    Console.WriteLine($"Число {B} самое большое число из списка");
+}
