19cbdc5862cd2f14f64a72a37692a2ecaef0a929
RussianLord <dj_volume@mail.ru>
2022-09-16 12:49:51 +0300

    Решил первое ДЗ

--- First/Program.cs
+++ First/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите ПЕРВОЕ число...");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Первое число = " + A);
+Console.WriteLine("Введите ВТОРОЕ число...");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число = " + B);
+
+if(A>B)
+{
+    Console.WriteLine($"Число {A} больше числа {B}");
+}
+else
+{
+    Console.WriteLine($"Число {B} больше числа {A}");
+}
