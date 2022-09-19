74b1aa7d42ff8670a9872f4bfe2d1657042a64c7
RussianLord <dj_volume@mail.ru>
2022-09-19 20:18:55 +0300

    Второй вариант для second

--- Second/Program.cs
+++ Second/Program.cs
@@ -1,4 +1,46 @@
-﻿Console.WriteLine("Программа показывает наибольшее число из введённых");
+﻿// Console.WriteLine("Программа показывает наибольшее число из введённых");
+// Console.WriteLine("Введите ПЕРВОЕ число...");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Число А = " + A);
+// Console.WriteLine("Введите ВТОРОЕ число...");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Число В = " + B);
+// Console.WriteLine("Введите ТРЕТЬЕ число...");
+// int C = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Число С = " + C);
+
+// if(A == B & A == C)
+// {
+//     Console.WriteLine("Вы ввели одинаковые числа");
+// }
+// else if(A == B & A > C)
+// {
+//     Console.WriteLine($"Число A = {A} и число B = {B} самые большые числа из списка");
+// }
+// else if(A == C & A > B)
+// {
+//     Console.WriteLine($"Число A = {A} и число C = {C} самые большые числа из списка");
+// }
+// else if(B == C & B > A)
+// {
+//     Console.WriteLine($"Число B = {B} и число C = {C} самые большые числа из списка");
+// }    
+// else if(A > B & A > C)
+// {
+//     Console.WriteLine($"Число {A} самое большое число из списка");
+// }
+// else if(C > B & C > A)
+// {
+//     Console.WriteLine($"Число {C} самое большое число из списка");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {B} самое большое число из списка");
+// }
+
+//// Варианта 2
+
+Console.WriteLine("Программа показывает наибольшее число из введённых");
 Console.WriteLine("Введите ПЕРВОЕ число...");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Число А = " + A);
@@ -8,32 +50,14 @@ Console.WriteLine("Число В = " + B);
 Console.WriteLine("Введите ТРЕТЬЕ число...");
 int C = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Число С = " + C);
+int max = A;
 
-if(A == B & A == C)
+if (B > max)
 {
-    Console.WriteLine("Вы ввели одинаковые числа");
+    max = B;
 }
-else if(A == B & A > C)
+if (C > max)
 {
-    Console.WriteLine($"Число A = {A} и число B = {B} самые большые числа из списка");
+    max = C;
 }
-else if(A == C & A > B)
-{
-    Console.WriteLine($"Число A = {A} и число C = {C} самые большые числа из списка");
-}
-else if(B == C & B > A)
-{
-    Console.WriteLine($"Число B = {B} и число C = {C} самые большые числа из списка");
-}    
-else if(A > B & A > C)
-{
-    Console.WriteLine($"Число {A} самое большое число из списка");
-}
-else if(C > B & C > A)
-{
-    Console.WriteLine($"Число {C} самое большое число из списка");
-}
-else
-{
-    Console.WriteLine($"Число {B} самое большое число из списка");
-}
+Console.WriteLine(max);
